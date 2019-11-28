hedgedoc/hedgedoc

Add allowAnonymousViews option
resolve #1144
set to `false` to delete `freely`, `editable` and `locked` permission when allowAnonymous is true (default is `true`)
Signed-off-by: TAKIMOTO, Atsushi <hakoai64@gmail.com>

lib/config/index.js
@@ -68,11 +68,17 @@ if (config.ldap.tlsca) {
 
 // Permission
 config.permission = Permission
-if (!config.allowAnonymous && !config.allowAnonymousEdits) {
+let defaultParmission = config.permission.editable
+if (!config.allowAnonymous && !config.allowAnonymousViews) {
+  delete config.permission.freely
+  delete config.permission.editable
+  delete config.permission.locked
+  defaultParmission = config.permission.limited
+} else if (!config.allowAnonymous && !config.allowAnonymousEdits) {
   delete config.permission.freely
 }
 if (!(config.defaultPermission in config.permission)) {
-  config.defaultPermission = config.permission.editable
+  config.defaultPermission = defaultParmission
 }
 
 // cache result, cannot change config in runtime!!!

isabella232/codimd-cloudgov-template

Signed-off-by: hakoai <hakoai64@gmail.com>

lib/config/index.js
@@ -68,11 +68,17 @@ if (config.ldap.tlsca) {
 
 // Permission
 config.permission = Permission
-if (!config.allowAnonymous && !config.allowAnonymousEdits) {
+let defaultPermission = config.permission.editable
+if (!config.allowAnonymous && !config.allowAnonymousViews) {
+  delete config.permission.freely
+  delete config.permission.editable
+  delete config.permission.locked
+  defaultPermission = config.permission.limited
+} else if (!config.allowAnonymous && !config.allowAnonymousEdits) {
   delete config.permission.freely
 }
 if (!(config.defaultPermission in config.permission)) {
-  config.defaultPermission = config.permission.editable
+  config.defaultPermission = defaultPermission
 }
 
 // cache result, cannot change config in runtime!!!